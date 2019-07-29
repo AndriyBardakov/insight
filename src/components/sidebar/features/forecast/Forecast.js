88e433279b57c87c6999a08994c8e7053a39fd4d
Andriy <andriybardakov@MacBook-Pro-Andriy.local>
2019-07-29 11:02:24 +0300

    updates for Forecast feature

--- src/components/sidebar/features/forecast/Forecast.js
+++ src/components/sidebar/features/forecast/Forecast.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Form, Button } from 'semantic-ui-react';
 
-class Forecast extends React.Component{
+class Forecast extends React.Component {
+    state = { accuracy: null };
 
-    render(){
+    onChangeAccuracy = (e) => {
+        this.setState({ accuracy: e.target.value });
+    };
+
+    onSubmitHandler = () => {
+        const accuracy = this.state.accuracy;
+        //TODO: set accuracy
+    };
+
+    render() {
         return (
-            <Form className="sidebar-content feature-form">
-                <Button primary type='submit'>Calculate accuracy</Button>
-                <Form.Field type='number' min='0' control='input'  />
+            <Form className="sidebar-content feature-form" onSubmit={this.onSubmitHandler}>
+                <Button primary type='submit' >Calculate accuracy</Button>
+                <Form.Field type='number' min='0' control='input' onChange={this.onChangeAccuracy} value={this.state.accuracy} />
             </Form>
         );
     }
